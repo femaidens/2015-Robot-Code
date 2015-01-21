40f76d1fe7cfd64ca69a349eef50f67e28e17ea0
Chelsea Wang <wangc9@bxscience.edu>
2015-01-21 17:11:34 -0500

    Update Forklift.java

--- Forklift.java
+++ Forklift.java
@@ -1,8 +1,8 @@
-/* MUST CREATE ARM CLASS FROM MOTOR AND PULLEY
- * CREATE UP AND DOWN METHODS IN MOTORS OR PULLEY
- * MOTORS FORWARD OR BACKWARD. POSITIVE OR NEGATIVE
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
- 
 package edu.wpi.first.wpilibj.templates;
 
 //the forklift that will carry the totes and recycling bin
