78779eec6b710f0e9d04ed7ed74109863f28b30a
karennguyen <karennguyen@users.noreply.github.com>
2015-01-23 17:20:54 -0500

    Update MecanumDrive.java

--- src/org/usfirst/frc/team2265/robot/subsystems/MecanumDrive.java
+++ src/org/usfirst/frc/team2265/robot/subsystems/MecanumDrive.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class MecanumDrive extends Subsystem {
+	//creation of talon motors, mecDrive, and gyroscope
 		TalonSRX frontLeft = new TalonSRX(1);
 		TalonSRX rearLeft = new TalonSRX(2);
 		TalonSRX frontRight = new TalonSRX(3);
@@ -17,8 +18,7 @@ public class MecanumDrive extends Subsystem {
 		RobotDrive mecDrive = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
 		Gyro gyroscope = new Gyro(1);
 	
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+    // Put methods for controlling this subsystem here. Call these from Commands.
      public void drive() {
     	 double xValue = 0.0;
     	 double yValue = 0.0;
@@ -26,9 +26,11 @@ public class MecanumDrive extends Subsystem {
     	 double gyroAngle = 0.0;
         
     	 mecDrive.mecanumDrive_Cartesian(oi.leftJoystick.getX(), oi.rightJoystick.getY(), oi.leftJoystick.getX(), gyroscope.getAngle());
+    //mecanum cartesian drive command
      }
 
     public void initDefaultCommand() {
+    	//the default command that is used when MecanumDrive is initialized
     	frontLeft.set(0.0);
     	frontRight.set(0.0);
     	rearLeft.set(0.0);
