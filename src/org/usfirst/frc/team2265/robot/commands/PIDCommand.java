742472ec95f549cf9ffd42bc5e0b7a8cacda697f
gloriangan <ngang@bxscience.edu>
2015-01-23 17:22:43 -0500

    Create PIDCommand.java

---
+++ src/org/usfirst/frc/team2265/robot/commands/PIDCommand.java
@@ -0,0 +1,39 @@
+
+package org.usfirst.frc.team2265.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+//import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+import org.usfirst.frc.team2265.robot.Robot;
+
+/**
+ *
+ */
+ 
+public class PIDCommand extends Command {	
+	
+    public PIDcommand() {		// Set values????
+        // commands depends on PID subsystem
+        requires(Robot.PID);		
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {}
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	 Robot.PID.enable();		// Starts PID
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;		// This should always be false, PID runs as long as the robot is on
+    }
+
+    // Called once after isFinished returns true (which is never)
+    // PID is always running, no interrupt needed
+    protected void end() {}
+
+    // PID is always running, no interrupt needed
+    protected void interrupted() {}
+}
