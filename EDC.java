fecae5523b822c0950f7d84d22a0adffd1a60b6d
mahmuds2 <mahmuds2@bxscience.edu>
2015-01-23 15:54:11 -0500

    Update EDC.java

--- EDC.java
+++ EDC.java
@@ -7,10 +7,9 @@
 /* VARIABLE NAMES:
  * b = base
  * c = crosspoint
- * h = vertical scaling of exp function 
- * k = vertical scaling of log function
+ * h = vertical dilation of exp function 
+ * k = vertical dilation of log function
  */
-
 
 import edu.wpi.first.wpilibj.Joystick;
 import java.lang.Math;
@@ -24,34 +23,36 @@ public class EDC{
   }
 
   public double convertDriveVal(double yValue){
-    // get y-value of given joystick
+    // get y-value of given joystick and intialize driveVal
     double yVal = yValue;
+    double driveVal;
     
-    //when joystick isn't moving
+    //return 0 when joystick isn't moving
     if (yVal == 0){
       return 0.0;
     }
-    /* if backwards, convert to drive value with f(x) = -log(-1(x-1)) 
-    else if (yVal < -0.5){
-      return (double)(-1)*Math.log10(-1*(yVal-1)); 
-    } 
     
-    else if (yVal < 0.0 && yVal > -0.5){
-      //return {some exponential function} 
-    } */
+    //convert y-value to positive by making it the absolute value
+    double y = Math.abs(yVal);
     
-    //if forwards, convert to drive value with f(x) = log(b, 1-c+x)
-    else if (yVal >= CROSSOVER){
+    //convert to drive value with f(x) = log(b, 1-c+x)
+    if ( y >= CROSSOVER ){
       double k = (1 - CROSSOVER) / (logN(BASE, 2 - CROSSOVER) - 1);
-      return Math.log(yVal + 1 - CROSSOVER) * k + CROSSOVER; 
+      driveVal = Math.log(yVal + 1 - CROSSOVER) * k + CROSSOVER; 
     }
     
-    //if forwards and y value between 0 and 0.5, convert with f(x) = (b^c-1)h+c
-    else if (yVal < CROSSOVER && yVal > 0){
+    //y value between 0 and 0.5, convert with f(x) = (b^c-1)h+c
+    else if ( y < CROSSOVER && y > 0 ){
       double h = (CROSSOVER) / (Math.pow(BASE, CROSSOVER) - 1);
-      return (Math.pow(BASE, CROSSOVER) - 1 * h);
-      
+      driveVal = (Math.pow(BASE, CROSSOVER) - 1 * h);
     }
+    
+    //make drive value negative if y-value was originally negative
+    if( yVal < 0 ){
+      driveVal = driveVal * -1;
+    }
+    
+    return driveVal;
   }
 }  
   
