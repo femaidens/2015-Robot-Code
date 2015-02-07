f4dc299efc0e6432a87429ed511dca59d5a3456e
gloriangan <raptorrix@gmail.com>
2015-02-07 11:21:30 -0500

    Added start compressor method.

--- src/org/usfirst/frc/team2265/robot/subsystems/Manipulator.java
+++ src/org/usfirst/frc/team2265/robot/subsystems/Manipulator.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team2265.robot.subsystems;
 
+import org.usfirst.frc.team2265.robot.Robot;
 import org.usfirst.frc.team2265.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -10,10 +13,21 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
  * to fit either the bin or totes. The extend(), retract(), and off() methods
  * are for controlling the piston.
  */
+
 public class Manipulator extends Subsystem {
 	//Subsystem device
 	DoubleSolenoid piston = new DoubleSolenoid(RobotMap.pistonPortOne, RobotMap.pistonPortTwo);
-
+	Compressor compressor = new Compressor();
+	
+	/**
+	 * Starts the compressor for pneumatics.
+	 */
+	public void start() {
+		if (Robot.isReal()) {
+			compressor.start();
+		}
+	}
+	
 	/*
 	 * method used to extend the pistons
 	 * @see edu.wpi.first.wpilibj.command.Subsystem#initDefaultCommand()
@@ -50,8 +64,5 @@ public class Manipulator extends Subsystem {
 	 * no default command
 	 * @see edu.wpi.first.wpilibj.command.Subsystem#initDefaultCommand()
 	 */
-	@Override protected void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	protected void initDefaultCommand() {}
 }
