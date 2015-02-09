d98a81e69102779db0b4736c5ca8ac746434a5ca
mahmuds2 <mahmuds2@013-35.bxsci.78x445.nycboe.org>
2015-02-09 16:40:09 -0500

    changes to edci final

--- src/org/usfirst/frc/team2265/robot/subsystems/EDC.java
+++ src/org/usfirst/frc/team2265/robot/subsystems/EDC.java
@@ -15,22 +15,24 @@ public class EDC extends Subsystem {
   * higher base makes gentler average rate of change */
   private final double BASE = 10; 
     
- /* create left joystick */ 
-  //Joystick leftJoy;
+ /* create Joystick leftJoy */
   Joystick driveJoy; 
+  
+  public EDC(){
+  }
  
- /* change of base formula for log function */
+ /* change of base formula for log function*/
  private double logN(double base, double n){
   return Math.log(n)/Math.log(base);
   }
  
  /* method converts y-value to drive value */
- public double convertDriveVal(double yValue){
+ public double convertDriveVal(){
   
    /* get y-value of given joystick and intialize driveVal */ 
    //double yVal = leftJoy.getY();
-	 double yVal = driveJoy.getRawAxis(2);
-	 double driveVal = 1; // TODO this might not be correct
+  double yVal = driveJoy.getRawAxis(2);
+  double driveVal = 1; // TODO this might not be correct
      
    /* return 0 when joystick isn't moving */
    if (yVal == 0){
@@ -42,21 +44,21 @@ public class EDC extends Subsystem {
      
    /* convert to drive value with f(x) = log(b, 1-c+x)*/
    if ( y >= CROSSOVER ){
-     double k = (1 - CROSSOVER) / (logN(BASE, 2 - CROSSOVER) - 1);
-     driveVal = Math.log(yVal + 1 - CROSSOVER) * k + CROSSOVER; 
+     double h = (1 - CROSSOVER) / (this.logN(BASE, 2 - CROSSOVER));
+     driveVal = this.logN(BASE, y + 1 - CROSSOVER) * h + CROSSOVER; 
    }
      
    /* y value between 0 and 0.5, convert with f(x) = (b^c-1)h+c */
    else if ( y < CROSSOVER && y > 0 ){
-     double h = (CROSSOVER) / (Math.pow(BASE, CROSSOVER) - 1);
-     driveVal = (Math.pow(BASE, CROSSOVER) - 1 * h);
+     double h = this.logN(BASE, CROSSOVER + 1) / CROSSOVER;
+     driveVal = Math.pow(BASE, (y * h)) - 1;
    }
    
    /* make drive value negative if y-value was originally negative */
    else if( yVal < 0 ){
      driveVal = driveVal * -1;
    }
-	   return driveVal;
+    return driveVal;
  }
 
  public void initDefaultCommand() {
