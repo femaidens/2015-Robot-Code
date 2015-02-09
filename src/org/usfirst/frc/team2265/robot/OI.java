a149ff1a2166595492d2ba71ac7af31ca4513142
Chelsea Wang <wangc9@bxscience.edu>
2015-02-09 16:03:46 -0500

    comments and spacing

--- src/org/usfirst/frc/team2265/robot/OI.java
+++ src/org/usfirst/frc/team2265/robot/OI.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2265.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button; //These are class imports  
+//These are class imports  
+import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.Joystick;
 import org.usfirst.frc.team2265.robot.commands.BinMode;
@@ -12,13 +13,10 @@ import org.usfirst.frc.team2265.robot.commands.ToteMode;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-
-//OI's back. tell a friend.
-//another random edit
-
+ 
 public class OI {
-	//public static Joystick leftJoy= new Joystick(RobotMap.leftJoyPort);
-	//public static Joystick rightJoy=new Joystick(RobotMap.rightJoyPort);
+	//public static Joystick leftJoy = new Joystick(RobotMap.leftJoyPort);
+	//public static Joystick rightJoy = new Joystick(RobotMap.rightJoyPort);
 	public static Joystick attackJoy = new Joystick(RobotMap.atkJoyPort);
  
 	Button pistonOutButton = new JoystickButton(attackJoy, RobotMap.pistonOutNum),
@@ -27,14 +25,14 @@ public class OI {
 		forkliftDownButton = new JoystickButton(attackJoy, RobotMap.forkliftDownNum);
 
 	public OI (){
+		// When the piston out button is pressed, the robot switches to Tote Mode.
 		pistonOutButton.whenPressed(new ToteMode());
+		// When the piston in button is pressed, the robot swicthes to Bin Mode.
 		pistonInButton.whenPressed(new BinMode());
+		// When the forklift up button is pressed, the forklift moves up.
 		forkliftUpButton.whenPressed(new ForkliftUp());
+		// When the forklift down button is pressed, the forklift moves down.
 		forkliftDownButton.whenPressed(new ForkliftDown());
-
-		//So the code is for buttons
-		//
-
 	}
 }
 // One type of button is a joy stick button which is any button on a joy stick.
